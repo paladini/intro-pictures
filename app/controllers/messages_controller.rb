75edb40be7fcc4f930c5ccb7c3c6e9a9c7b23eb8
Fernando Paladini <fnpaladini@gmail.com>
2016-05-16 02:22:43 -0300

    Remove team section when none employee was inserted.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,11 +7,19 @@ class MessagesController < ApplicationController
 	def create
 	    @message = Message.new(message_params)
 
-	    if verify_recaptcha(model: @message) && @message.valid?
+	    if verify_recaptcha(model: @message) and @message.valid?
 	      	MessageMailer.message_me(@message).deliver_now
-	      	redirect_to new_message_path, notice: "Thank you for your message."
+	      	redirect_to root_path, notice: "Thank you for the message."
+	      	# redirect_to new_message_path, notice: "Thank you for your message."
 	    else
+	    	# redirect_to root_path
+	    	# flash[]
+	    	# render root_path
+	    	# render 'pages/home'
 	      	render :new
+	      	# flash = flash.now
+	      	# flash[:error] = @message.errors.full_messages.to_sentence
+	      	# redirect_to :back
 	    end
   	end
 
