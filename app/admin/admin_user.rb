4253dda6c98cdea689888fde07a0279e7b8098ef
Fernando Paladini <fnpaladini@gmail.com>
2016-05-16 12:40:34 -0300

    Administrators resource done.

--- app/admin/admin_user.rb
+++ app/admin/admin_user.rb
@@ -1,21 +1,18 @@
-ActiveAdmin.register AdminUser do
+ActiveAdmin.register AdminUser, as: "administrador" do
 
-  menu label: "Admins"
+  menu label: "Administradores"
   permit_params :email, :password, :password_confirmation
 
-  index do
+  index title: "Administradores" do
     selectable_column
     id_column
     column :email
     column :current_sign_in_at
-    column :sign_in_count
     column :created_at
     actions
   end
 
   filter :email
-  filter :current_sign_in_at
-  filter :sign_in_count
   filter :created_at
 
   form do |f|
