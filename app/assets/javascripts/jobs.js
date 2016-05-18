6efbe8bd08828c1d1e80d43703cae78291b835af
Fernando Paladini <fnpaladini@gmail.com>
2016-05-18 13:21:19 -0300

    Modal responsiveness 100%.

--- app/assets/javascripts/jobs.js
+++ app/assets/javascripts/jobs.js
@@ -1,17 +1,39 @@
+var myOwnDelay = (function(){
+  var timer = 0;
+  return function(callback, ms){
+    clearTimeout (timer);
+    timer = setTimeout(callback, ms);
+  };
+})();
+
+var setModalHeight = function() {
+	if (($('.modal-body').length > 0) && !($('.modal-body').hasClass('locked-body'))) {
+		if ($(window).width() >= 992) {
+			var vimeoHeight = $('.video').last().height().toString() + "px !important";
+			$('.col-video-description').last().attr("style", 'height:' + vimeoHeight);
+		}
+	}
+}
+
 var parseModal = function() {
 
 	$('.job-outer').on('click', function(e){
 		$('#myModal').modal('show');
+		myOwnDelay(function(){
+			setModalHeight();
+	    }, 150);
 	});
 
 	$("#myModal").on('hidden.bs.modal', function (e) {
 	    $("#myModal iframe").attr("src", $("#myModal iframe").attr("src"));
 	});
 
-	// For locked content
-	// $('#unlock_password_button').on('click', function(e){
-	// 	console.log($('#unlock_password_text').val());
-	// });
+	// Modal responsiveness
+	$(window).resize(function() {
+	    myOwnDelay(function(){
+			setModalHeight();
+	    }, 150);
+	});
 
 }
 $(document).ready(parseModal);
